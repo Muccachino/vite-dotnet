cffc7ef98127565fe46664c444c6332225047849
Muccachino <lucas_staszewski@yahoo.de>
2024-05-02 12:24:12 +0200

    add Validation to Department Create Page

--- ContosoUniversityBackend/ContosoUniversityBackend/clientapp/src/components/Departments/DepartmentCreate.tsx
+++ ContosoUniversityBackend/ContosoUniversityBackend/clientapp/src/components/Departments/DepartmentCreate.tsx
@@ -1,15 +1,14 @@
 import Header from "../Header.tsx";
 import Footer from "../Footer.tsx";
-import {Button, Divider, FormLabel, Input} from "@mui/joy";
+import {Button, Divider, FormLabel} from "@mui/joy";
 import {Link, useNavigate} from "react-router-dom";
 import {IDepartment, IInstructor} from "../../interfaces/global_interfaces.ts";
-import  {useState} from "react";
+import {ChangeEvent, useState} from "react";
 import useDepartments from "../../useData/useDepartments.ts";
-import {Box, FormControl, MenuItem, Select} from "@mui/material";
+import {Box, FormControl, MenuItem, TextField} from "@mui/material";
 import useInstructors from "../../useData/useInstructors.ts";
 
 
-//TODO: Administrator / Instructor Object input not working
 
 export default function DepartmentCreate() {
   const navigate = useNavigate();
@@ -17,10 +16,54 @@ export default function DepartmentCreate() {
   const [instructors] = useInstructors() as [instructors: IInstructor[]];
   const [currentDepartment, setCurrentDepartment] = useState<IDepartment>({
     name: "",
-    startDate: "2024-01-01",
+    startDate: "",
     budget: 0,
     instructorID: 0,
   });
+  const [nameError, setNameError] = useState<boolean>(false);
+  const [budgetError, setBudgetError] = useState<boolean>(false);
+  const [adminError, setAdminError] = useState<boolean>(false);
+  const [dateError, setDateError] = useState<boolean>(false);
+  const [createError, setCreateError] = useState<boolean>(false);
+
+function handleNameChange(e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) {
+  setCreateError(false);
+  setCurrentDepartment(prevState => ({...prevState, name: (e.target.value)}))
+  e.target.validity.valid ? setNameError(false) : setNameError(true);
+}
+
+function handleBudgetChange(e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>){
+  setCreateError(false);
+  setCurrentDepartment(prevState => ({...prevState, budget: parseInt(e.target.value)}))
+  e.target.validity.valid ? setBudgetError(false) : setBudgetError(true);
+}
+
+function handleAdministratorChange(e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>){
+  setCreateError(false);
+  setCurrentDepartment(prevState => ({...prevState, instructorID: parseInt(e.target.value as string)}))
+  parseInt(e.target.value) !== 0 ? setAdminError(false) : setAdminError(true);
+}
+
+function handleDateChange(e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>){
+  setCreateError(false);
+  setCurrentDepartment(prevState => ({...prevState, startDate: (e.target.value)}))
+  e.target.validity.valid ? setDateError(false) : setDateError(true);
+}
+
+function handleSubmit(){
+  if(!nameError && !budgetError &&
+    !adminError &&
+    !dateError &&
+    currentDepartment.name !== "" &&
+    currentDepartment.instructorID !== 0 &&
+    currentDepartment.budget !== 0 &&
+    currentDepartment.startDate !== ""){
+    (createDepartment as (departmentToCreate: IDepartment) => Promise<void>)(currentDepartment);
+    navigate("/departments")
+  } else {
+    setCreateError(true);
+  }
+}
 
   return (
     <>
@@ -31,40 +74,67 @@ export default function DepartmentCreate() {
           <h3>Department</h3>
         </div>
 
-        <div style={{ height: "250px", width: "30vw", display: "flex", flexDirection: "column", justifyContent: "space-between"}}>
+        <div style={{ width: "30vw", display: "flex", flexDirection: "column", justifyContent: "space-between"}}>
           <Divider sx={{marginBottom: "20px"}}/>
 
           <FormLabel>Department Name</FormLabel>
-          <Input onChange={(e) => setCurrentDepartment(prevState => ({...prevState, name: (e.target.value)}))}/>
+          <TextField
+            hiddenLabel
+            size={"small"}
+            variant="outlined"
+            required
+            onChange={(e) => handleNameChange(e)}
+            error={nameError}
+            helperText={nameError ? "Please enter your name (letters and spaces only)" : ""}
+            inputProps={{pattern: "[A-Za-z ]+",}}
+          />
           <FormLabel>Administrator</FormLabel>
           <Box sx={{ minWidth: 120 }}>
             <FormControl fullWidth>
-              <Select
-                labelId="demo-simple-select-label"
-                id="demo-simple-select"
+              <TextField
+                select
+                required
                 size={"small"}
-                value={currentDepartment.instructorID}
-                onChange={(e) => setCurrentDepartment(prevState => ({...prevState, InstructorId: parseInt(e.target.value as string)}))}
+                error={adminError}
+                helperText={adminError ? "Choose an Administrator!" : ""}
+                value={currentDepartment.instructorID?.toString()}
+                onChange={(e) => handleAdministratorChange(e)}
               >
+                <MenuItem value={0}>-- Choose an Administrator --</MenuItem>
                 {instructors.map((instructor, index) => {
                   return (
                     <MenuItem key={index} value={instructor.id}>{instructor.firstMidName + " " + instructor.lastName}</MenuItem>
                   )
                 })}
-              </Select>
+              </TextField>
             </FormControl>
           </Box>
           <FormLabel>Budget</FormLabel>
-          <Input onChange={(e) => setCurrentDepartment(prevState => ({...prevState, budget: parseInt(e.target.value)}))}/>
+          <TextField
+            hiddenLabel
+            size={"small"}
+            variant="outlined"
+            required
+            onChange={(e) => handleBudgetChange(e)}
+            error={budgetError}
+            helperText={budgetError ? "Numbers Only!" : ""}
+            inputProps={{pattern: "^[0-9]{1,45}$",}}/>
+
           <FormLabel>Implementation</FormLabel>
-          <Input type={"date"} onChange={(e) => setCurrentDepartment(prevState => ({...prevState, startDate: (e.target.value)}))}/>
+          <TextField
+            required
+            size={"small"}
+            type={"date"}
+            onChange={(e) => handleDateChange(e)}
+            error={dateError}
+            helperText={dateError ? "Please enter a valid date" : ""}/>
         </div>
 
         <div>
-          <Button sx={{marginTop: "30px"}} onClick={() => {
-            (createDepartment as (departmentToCreate: IDepartment) => Promise<void>)(currentDepartment);
-            navigate("/departments")}}>Save</Button>
+          <Button sx={{marginTop: "30px"}} onClick={() => handleSubmit()}>Save</Button>
           <span style={{marginLeft: "30px"}}><Link to={"/departments"}>Back to List</Link></span>
+          {createError &&
+              <p style={{color: "red", marginTop: "20px"}}>One or more inputs are invalid !</p>}
         </div>
       </div>
       <Footer/>
